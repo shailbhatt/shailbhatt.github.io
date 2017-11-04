63d268a9a0852c17b19749af95bc80c12133edd1
Kapeel Kokane <kapeel.kokane@gmail.com>
2017-11-04 20:36:12 +0530

    uploading bootstrap files

---
+++ assets/js/controller/controller.js
@@ -0,0 +1,9 @@
+ app = angular.module("myApp", []);
+
+    app.controller("myCtrl", function($scope) {
+
+    $scope.firstName = "Shail";
+    $scope.lastName = "Bhatt";
+
+    $scope.designation = "Full Stack Developer";
+    });
